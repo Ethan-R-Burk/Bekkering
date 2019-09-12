74e7b5674697103d51764e3e259c0576d5607a70
Ethan-R-Burk <54331592+Ethan-R-Burk@users.noreply.github.com>
2019-09-12 10:08:33 -0500

    Add files via upload
    Here is the partially completed assignment.

---
+++ Assets/Nail.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nail : MonoBehaviour
+{
+    private bool isNailed = false;
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    void Update()
+    {
+        if(!isNailed)
+        rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Rotator")
+        {
+            transform.SetParent(col.transform);
+            isNailed = true;
+        }
+        else if (col.tag == "Nail")
+        {
+            FindObjectOfType <GameManager>().EndGame();
+        }
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject nailPrefab;
+
+    void Update()
+    {
+       if (Input.GetButtonDown("Fire1"))
+        {
+            SpawnNail();
+        }
+    }
+
+    void SpawnNail()
+    {
+        Instantiate(nailPrefab, transform.position, transform.rotation);
+    }
+
+}
